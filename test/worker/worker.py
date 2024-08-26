8fbe6fdb549fe7b60cc4cfd8c102b7b3cc14a4af
Katehok <a.timin2003@mail.ru>
2024-08-26 22:17:25 +0300

    Added PosgresSQL into simple connection

--- test/worker/worker.py
+++ test/worker/worker.py
@@ -1,6 +1,8 @@
 import time
 import pika
 import logging
+import psycopg2
+from psycopg2 import sql
 
 # Настройка логирования
 logging.basicConfig(
@@ -9,8 +11,20 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s'  # Формат записи
 )
 
+def save_to_db(connection, data):
+    try:
+        with connection.cursor() as cursor:
+            query = sql.SQL("INSERT INTO messages (content) VALUES (%s)")
+            cursor.execute(query, (data,))
+            connection.commit()
+    except Exception as e:
+        logging.error(f"Failed to save data to DB: {e}")
+
 def callback(ch, method, properties, body):
     logging.info(f"Received {body.decode()}")  # Запись в лог
+
+    # обработка заявок
+    save_to_db(db_connection, body.decode())
     
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
@@ -18,12 +32,24 @@ def connect_to_rabbitmq():
     while True:
         try:
             connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', credentials=pika.PlainCredentials('user', 'password')))
+            logging.info(f"Connected to the RabbitQM")  # Запись в лог
             return connection
         except pika.exceptions.AMQPConnectionError:
             logging.error("RabbitMQ not ready, retrying in 5 seconds...")
             time.sleep(5)
 
+def connect_to_db():
+    while True:
+        try:
+            connection = psycopg2.connect(dbname='mydatabase', user='myuser', password='mypassword', host='db')
+            logging.info(f"Connected to the PostgreSQL")  # Запись в лог
+            return connection
+        except psycopg2.OperationalError:
+            logging.error("PostgreSQL not ready, retrying in 5 seconds...")
+            time.sleep(5)
+
 connection = connect_to_rabbitmq()
+db_connection = connect_to_db()
 
 channel = connection.channel()
 channel.queue_declare(queue='task_queue', durable=True)

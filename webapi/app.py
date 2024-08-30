1528aa753660db2db82c29bd68af62a6309f5be4
Katehok <a.timin2003@mail.ru>
2024-08-30 09:46:06 +0300

    Finished lab 5

--- webapi/app.py
+++ webapi/app.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, make_response
 import pika
 import json
 
@@ -32,6 +32,20 @@ def index():
     # print("Rendering index page")
     return render_template('form.html', message='Форма успешно отправлена!')
 
+@app.route('/cached', methods=['GET'])
+def index_cached():
+    logging.info("Rendering cached page")  # Логирование обращения к главной странице
+    
+    # Рендеринг страницы
+    rendered_html = render_template('form.html', message='Форма успешно отправлена!')
+    
+    # Создание ответа и добавление заголовков для кэширования (так как страничка меняется, дай бог, раз в год, то и экспайр соответствующий)
+    response = make_response(rendered_html)
+    response.headers['Cache-Control'] = 'public, max-age=86400'  # Кэшировать страницу на 1 сутки (86400 секунд)
+    response.headers['Expires'] = 'Thu, 31 Dec 2024 23:59:59 GMT'  # Устанавливаем фиксированную дату истечения кэша
+    
+    return response
+
 @app.route('/send', methods=['POST'])
 def send_message():
     message = request.json
@@ -39,6 +53,14 @@ def send_message():
     logging.info(f"Received message: {message}")  # Логирование полученного сообщения
     # print(f"Received message: {message}")
     return redirect(url_for('index'))
+
+@app.route('/send_cached', methods=['POST'])
+def send_message_cached():
+    message = request.json
+    send_to_queue(message)
+    logging.info(f"Received message: {message}")  # Логирование полученного сообщения
+    # print(f"Received message: {message}")
+    return redirect(url_for('cached'))
 
 if __name__ == '__main__':
     logging.info(f"Waiting for http-requests...")

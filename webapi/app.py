5cda718470b6eb35a94939f1270d8d0f73e991ed
Katehok <a.timin2003@mail.ru>
2024-08-30 07:38:16 +0300

    Finished lab 4

--- webapi/app.py
+++ webapi/app.py
@@ -1,26 +1,45 @@
+import logging
 from flask import Flask, request, render_template, redirect, url_for
 import pika
 import json
 
 app = Flask(__name__)
 
+# Настройка логирования
+logging.basicConfig(
+    # filename='/var/log/webapi.log',  # Путь к файлу для логов
+    level=logging.DEBUG,             # Уровень логирования
+    format='%(asctime)s %(levelname)s %(message)s',  # Формат логов
+    datefmt='%Y-%m-%d %H:%M:%S'      # Формат даты и времени
+)
+
 def send_to_queue(message):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', credentials=pika.PlainCredentials('user', 'password')))
-    channel = connection.channel()
-    channel.queue_declare(queue='task_queue', durable=True)
-    channel.basic_publish(exchange='', routing_key='task_queue', body=json.dumps(message))
-    connection.close()
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', credentials=pika.PlainCredentials('user', 'password')))
+        channel = connection.channel()
+        channel.queue_declare(queue='task_queue', durable=True)
+        channel.basic_publish(exchange='', routing_key='task_queue', body=json.dumps(message))
+        connection.close()
+        logging.info(f"Message sent to queue: {message}")  # Логирование успешной отправки сообщения
+        # print((f"Message sent to queue: {message}"))
+    except Exception as e:
+        logging.error(f"Failed to send message to queue: {e}")  # Логирование ошибки
+        # print(f"Failed to send message to queue: {e}")
 
 @app.route('/', methods=['GET'])
 def index():
+    logging.info("Rendering index page")  # Логирование обращения к главной странице
+    # print("Rendering index page")
     return render_template('form.html', message='Форма успешно отправлена!')
 
 @app.route('/send', methods=['POST'])
 def send_message():
     message = request.json
     send_to_queue(message)
-
+    logging.info(f"Received message: {message}")  # Логирование полученного сообщения
+    # print(f"Received message: {message}")
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    logging.info(f"Waiting for http-requests...")
     app.run(host='0.0.0.0')
--- worker/worker.py
+++ worker/worker.py
@@ -4,11 +4,13 @@ import logging
 import psycopg2
 from psycopg2 import sql
 
+
 # Настройка логирования
 logging.basicConfig(
-    filename='worker.log',  # Имя файла лога
-    level=logging.INFO,     # Уровень логирования
-    format='%(asctime)s - %(levelname)s - %(message)s'  # Формат записи
+    # filename='/var/log/worker.log',  # Имя файла лога
+    level=logging.DEBUG,     # Уровень логирования
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Формат записи
+    datefmt='%Y-%m-%d %H:%M:%S'      # Формат даты и времени
 )
 
 def save_to_db(connection, data):
@@ -19,9 +21,11 @@ def save_to_db(connection, data):
             connection.commit()
     except Exception as e:
         logging.error(f"Failed to save data to DB: {e}")
+        # print(f"Failed to save data to DB: {e}")
 
 def callback(ch, method, properties, body):
     logging.info(f"Received {body.decode()}")  # Запись в лог
+    # print(f"Received {body.decode()}")
 
     # обработка заявок
     save_to_db(db_connection, body.decode())
@@ -33,9 +37,11 @@ def connect_to_rabbitmq():
         try:
             connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', credentials=pika.PlainCredentials('user', 'password')))
             logging.info(f"Connected to the RabbitQM")  # Запись в лог
+            # print(f"Connected to the RabbitQM")
             return connection
         except pika.exceptions.AMQPConnectionError:
             logging.error("RabbitMQ not ready, retrying in 5 seconds...")
+            # print("RabbitMQ not ready, retrying in 5 seconds...")
             time.sleep(5)
 
 def connect_to_db():
@@ -43,9 +49,11 @@ def connect_to_db():
         try:
             connection = psycopg2.connect(dbname='mydatabase', user='myuser', password='mypassword', host='db')
             logging.info(f"Connected to the PostgreSQL")  # Запись в лог
+            # print(f"Connected to the PostgreSQL")
             return connection
         except psycopg2.OperationalError:
             logging.error("PostgreSQL not ready, retrying in 5 seconds...")
+            # print("PostgreSQL not ready, retrying in 5 seconds...")
             time.sleep(5)
 
 connection = connect_to_rabbitmq()
@@ -58,4 +66,5 @@ channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='task_queue', on_message_callback=callback)
 
 logging.info('Waiting for messages...')
+# print('Waiting for messages...')
 channel.start_consuming()
